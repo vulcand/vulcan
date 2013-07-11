388e8f85406a17f0c492102178e57af5a57308c3
Sergey Obukhov <sergey.obykhov@mailgunhq.com>
2013-07-11 14:04:13 -0700

    added docstrings for auth module, separate handling for 4xx and 5xx responses

--- vulcan/auth.py
+++ vulcan/auth.py
@@ -1,3 +1,21 @@
+# -*- test-case-name: vulcan.test.test_auth -*-
+
+"""
+Authorization module for reverse proxy.
+
+>>> from vulcan auth import authorize
+>>>
+>>> request_params = {
+...     "username": "user",
+...     "password": "secret",
+...     "protocol": "http",
+...     "method": "get",
+...     "uri": "http://localhost/test",
+...     "length": 0
+...     }
+>>> d = authorize(request_params)
+"""
+
 from functools import partial
 
 from twisted.internet import defer, threads
@@ -18,6 +36,51 @@ CACHE = ExpiringDict(max_len=100, max_age_seconds=60)
 
 
 def authorize(request_params):
+    """Authorize request based on the parameters extracted from the request.
+
+    >>> request_params = {
+    ...     "username": "user",
+    ...     "password": "secret",
+    ...     "protocol": "http",
+    ...     "method": "get",
+    ...     "uri": "http://localhost/test",
+    ...     "length": 0
+    ...     }
+    >>> d = authorize(request_params)
+
+    Authorization server should return JSON with authorization token and
+    a string of upstream servers:
+
+    '{"auth_token": "abc", "upstream": "10.241.0.25:3000,10.241.0.25:3001"}'
+
+    The token is a string uniquely identifying the requester and
+    the upstream represents servers the request could be proxied to.
+    Successful response i.e. dictionary decoded from the JSON string is cached.
+
+    If authorization server response isn't a valid JSON string a Failure
+    instance with CommunicationFailed exception is returned. The error is
+    logged.
+
+    NOTE: no checks are made to make sure that the response has the
+    required fields or that the fields have a valid format.
+
+    Authorization server should return an error response if the request
+    couldn't be authorized, optionaly providing the reason e.g. as JSON:
+
+    '{"message": "Wrong API key"}'
+
+    If authorization server responds with 4xx code a Failure instance with
+    AuthorizationFailed exception is returned. The failure is cached.
+    The failure isn't logged.
+
+    If authorization server responds with 5xx code a Failure instance with
+    CommunicationFailed exception is returned. The failure isn't cached by
+    auth module. The response is logged.
+
+    If some unexpected error happens during request processing (e.g. if
+    authorization server is unavailable) a Failure instance with
+    CommunicationFailed exception is returned. The error is logged.
+    """
     hit = safe_format(
         "{username} {password} {protocol} {method} {uri} {data_size}",
         username=request_params["username"],
@@ -40,7 +103,7 @@ def authorize(request_params):
 
 
 def _errback(failure):
-    if isinstance(failure.value, AuthorizationFailed):
+    if isinstance(failure.value, (AuthorizationFailed, CommunicationFailed)):
         return failure
     else:
         log.err(failure)
@@ -48,16 +111,30 @@ def _errback(failure):
 
 
 def _authorization_received(hit, response):
-    if response.code >= 400:
+    if response.code >= 400 and response.code < 500:
         d = treq.content(response)
         d.addCallback(partial(_authorization_failed, hit, response.code))
         d.addErrback(partial(_failed_receive_auth_failure_reason,
                              hit, response.code))
+    elif response.code >= 500:
+        log.err(safe_format("Authorization for request {} failed with code {}",
+                            hit, response.code))
+        d = treq.content(response)
+        d.addCallback(partial(_authorization_failed_with_5xx,
+                              hit, response.code))
+        d.addErrback(_errback)
     else:
         d = treq.json_content(response)
         d.addCallback(partial(_authorization_succeeded, hit))
 
     return d
+
+
+def _authorization_failed_with_5xx(hit, code, reason):
+    log.err(safe_format(
+            "Authorization for request {} failed with code {} and reason {}",
+            hit, code, reason))
+    return Failure(CommunicationFailed())
 
 
 def _failed_receive_auth_failure_reason(hit, code, failure):
--- vulcan/test/test_auth.py
+++ vulcan/test/test_auth.py
@@ -10,7 +10,7 @@ from twisted.internet.error import ConnectionRefusedError
 from twisted.internet import defer
 from twisted.python.failure import Failure
 from twisted.web.client import ResponseDone, ResponseFailed
-from twisted.web.http import FORBIDDEN, OK
+from twisted.web.http import FORBIDDEN, SERVICE_UNAVAILABLE, BAD_GATEWAY, OK
 
 from vulcan import auth
 from vulcan.errors import CommunicationFailed, AuthorizationFailed, RESPONSES
@@ -44,7 +44,7 @@ class AuthTest(TestCase):
         self.assertEquals(auth.CACHE, {})
 
     @patch.object(treq, 'get')
-    def test_wrong_creds(self, treq_get):
+    def test_4xx(self, treq_get):
         self.setUpResponse(FORBIDDEN)
         treq_get.return_value = defer.succeed(self.response)
         d = auth.authorize(request_params())
@@ -61,7 +61,7 @@ class AuthTest(TestCase):
 
     @patch.object(treq, 'get')
     @patch.object(log, 'err')
-    def test_wrong_creds_fail_receive_reason(self, log_err, treq_get):
+    def test_4xx_fail_receive_response_body(self, log_err, treq_get):
         self.setUpResponse(FORBIDDEN)
         treq_get.return_value = defer.succeed(self.response)
         d = auth.authorize(request_params())
@@ -73,6 +73,54 @@ class AuthTest(TestCase):
         self.assertEqual(
             auth.CACHE.values()[-1].value,
             AuthorizationFailed(FORBIDDEN, RESPONSES[FORBIDDEN]))
+
+    @patch.object(treq, 'get')
+    def test_4xx_response_has_no_reason(self, treq_get):
+        self.setUpResponse(FORBIDDEN)
+        treq_get.return_value = defer.succeed(self.response)
+        d = auth.authorize(request_params())
+        self.protocol.connectionLost(Failure(ResponseDone()))
+        self.assertFailure(d, AuthorizationFailed)
+
+        # the failure was cached
+        self.assertEqual(
+            auth.CACHE.values()[-1].value,
+            AuthorizationFailed(FORBIDDEN, RESPONSES[FORBIDDEN], ''))
+
+    @patch.object(treq, 'get')
+    @patch.object(log, 'err')
+    def test_5xx(self, log_err, treq_get):
+        self.setUpResponse(BAD_GATEWAY)
+        treq_get.return_value = defer.succeed(self.response)
+        d = auth.authorize(request_params())
+        data = {"message": "Bad Gateway"}
+        self.protocol.dataReceived(json.dumps(data))
+        self.protocol.connectionLost(Failure(ResponseDone()))
+        self.assertFailure(d, CommunicationFailed)
+        # we log twice:
+        # * when we got the response code
+        # * when we got the response body
+        # this way if we fail to receive response body we'll still know that
+        # authorization failed with 5xx code
+        self.assertEquals(2, log_err.call_count)
+        self.assertIn(str(BAD_GATEWAY), log_err.call_args_list[0][0][0])
+        self.assertIn(json.dumps(data), log_err.call_args_list[1][0][0])
+
+    @patch.object(treq, 'get')
+    @patch.object(log, 'err')
+    def test_5xx_fail_receive_response_body(self, log_err, treq_get):
+        self.setUpResponse(BAD_GATEWAY)
+        treq_get.return_value = defer.succeed(self.response)
+        d = auth.authorize(request_params())
+        failure = Failure(ResponseFailed("Bam!"))
+        self.protocol.connectionLost(failure)
+        self.assertFailure(d, CommunicationFailed)
+        # we log twice:
+        # * we log that authorization failed with 5xx code
+        # * we log error that prevented us from getting the response body
+        self.assertEquals(2, log_err.call_count)
+        self.assertIn(str(BAD_GATEWAY), log_err.call_args_list[0][0][0])
+        self.assertEquals(failure, log_err.call_args_list[1][0][0])
 
     @patch.object(treq, 'get')
     @patch.object(log, 'err')
