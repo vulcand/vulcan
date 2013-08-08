e04a219bb660444a5a0f76d3d05c0a76ca98c636
Sergey Obukhov <sergey.obykhov@mailgunhq.com>
2013-08-08 14:59:48 -0700

    fix setup: expiringdict was made public, fix author, url information, added Apache 2 license

--- setup.py
+++ setup.py
@@ -22,18 +22,16 @@ setup(name='vulcan',
       version='1.0',
       description="reverse proxy service",
       long_description=open("README.md").read(),
-      author='Mailgun Inc.',
-      author_email='admin@mailgunhq.com',
-      url='http://www.mailgun.com',
-      license='MIT',
+      author='Sergey Obukhov',
+      author_email='sergey.obukhov@rackspace.com',
+      url='https://github.com/mailgun/vulcan',
+      license='APACHE2',
       packages=find_packages(exclude=['tests']),
       include_package_data=True,
       zip_safe=True,
       install_requires=[
         'setproctitle',
         'twisted',
-        # to convert strings to file-like objects
-        # 'Werkzeug==0.8.3',
         # required by telephus
         'pure-sasl==0.1.4',
         'expiringdict==1.0',
@@ -43,13 +41,11 @@ setup(name='vulcan',
         ],
       extras_require={'test': ['nose', 'mock', 'coverage']},
       dependency_links=[
-          ('https://{u}:{p}@github.com/mailgun/expiringdict/tarball/'
-           'master#egg=expiringdict-1.0').format(
-              u=quote(env.get('MG_COLABORATOR')),
-              p=quote(env.get('MG_COLABORATOR_PASSWORD'))),
-          ('https://github.com/driftx/Telephus/tarball/'
-           'master#egg=telephus-1.0.0-beta1')
-          ]
+        ('https://github.com/mailgun/expiringdict/tarball/'
+         'master#egg=expiringdict-1.0'),
+        ('https://github.com/driftx/Telephus/tarball/'
+         'master#egg=telephus-1.0.0-beta1')
+        ]
 
       )
 
@@ -58,6 +54,6 @@ if __name__ == '__main__':
     if any(cmd in sys.argv[1:] for cmd in ['develop', 'install', 'bdist_egg']):
         # adding github repo to dependency_links and requiring treq version
         # higher than on PyPi won't work for packages that require vulcan
-        # so w explicitly install it from github here
+        # so we explicitly install it from github here
         pip("install https://github.com/klizhentas/treq/tarball/"
             "b7e40c23108c810d81641d38a12e900bf4f2e599/master#egg=treq-0.2.0")
