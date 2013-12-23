7453fba1c1d9cf6964595135447c5f45380c95b7
Paul Querna <pquerna@apache.org>
2013-12-23 14:14:53 -0800

    Fix tests to include upstream metrics

--- command/upstream_test.go
+++ command/upstream_test.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"encoding/json"
+	"github.com/mailgun/vulcan/metrics"
 	. "launchpad.net/gocheck"
 )
 
@@ -16,10 +17,11 @@ func (s *UpstreamSuite) TestNewUpstream(c *C) {
 		5000)
 	c.Assert(err, IsNil)
 	expected := Upstream{
-		Id:     "http://localhost:5000",
-		Scheme: "http",
-		Host:   "localhost",
-		Port:   5000,
+		Id:      "http://localhost:5000",
+		Scheme:  "http",
+		Host:    "localhost",
+		Port:    5000,
+		Metrics: metrics.GetUpstreamMetrics("http_localhost_5000"),
 	}
 	c.Assert(*u, DeepEquals, expected)
 }
@@ -28,10 +30,11 @@ func (s *UpstreamSuite) TestNewUpstreamNoPort(c *C) {
 	u, err := NewUpstreamFromString("http://localhost")
 	c.Assert(err, IsNil)
 	expected := Upstream{
-		Id:     "http://localhost:80",
-		Scheme: "http",
-		Host:   "localhost",
-		Port:   80,
+		Id:      "http://localhost:80",
+		Scheme:  "http",
+		Host:    "localhost",
+		Port:    80,
+		Metrics: metrics.GetUpstreamMetrics("http_localhost_80"),
 	}
 	c.Assert(*u, DeepEquals, expected)
 }
@@ -44,37 +47,41 @@ func (s *UpstreamSuite) TestUpstreamFromObj(c *C) {
 		{
 			Parse: `"http://google.com:5000"`,
 			Expected: Upstream{
-				Id:     "http://google.com:5000",
-				Scheme: "http",
-				Host:   "google.com",
-				Port:   5000,
+				Id:      "http://google.com:5000",
+				Scheme:  "http",
+				Host:    "google.com",
+				Port:    5000,
+				Metrics: metrics.GetUpstreamMetrics("http_google_com_5000"),
 			},
 		},
 		{
 			Parse: `"http://google.com:5000/"`,
 			Expected: Upstream{
-				Id:     "http://google.com:5000",
-				Scheme: "http",
-				Host:   "google.com",
-				Port:   5000,
+				Id:      "http://google.com:5000",
+				Scheme:  "http",
+				Host:    "google.com",
+				Port:    5000,
+				Metrics: metrics.GetUpstreamMetrics("http_google_com_5000"),
 			},
 		},
 		{
 			Parse: `{"scheme": "http", "host": "localhost", "port": 3000}`,
 			Expected: Upstream{
-				Id:     "http://localhost:3000",
-				Scheme: "http",
-				Host:   "localhost",
-				Port:   3000,
+				Id:      "http://localhost:3000",
+				Scheme:  "http",
+				Host:    "localhost",
+				Port:    3000,
+				Metrics: metrics.GetUpstreamMetrics("http_localhost_3000"),
 			},
 		},
 		{
 			Parse: `{"scheme": "https", "host": "localhost", "port": 4000}`,
 			Expected: Upstream{
-				Id:     "https://localhost:4000",
-				Scheme: "https",
-				Host:   "localhost",
-				Port:   4000,
+				Id:      "https://localhost:4000",
+				Scheme:  "https",
+				Host:    "localhost",
+				Port:    4000,
+				Metrics: metrics.GetUpstreamMetrics("https_localhost_4000"),
 			},
 		},
 	}
